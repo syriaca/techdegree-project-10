a4b7abbe3df6800f03abfd006743c6f1444175af
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-14 17:04:23 +0200

    Models
    Initialize express project with sequelize and create a database model for books, patrons & loans

---
+++ models/loans.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Loans = sequelize.define('Loans', {
+    id: DataTypes.INTEGER,
+    book_id: DataTypes.INTEGER,
+    patron_id: DataTypes.INTEGER,
+    loaned_on: DataTypes.DATE,
+    return_by: DataTypes.DATE,
+    returned_on: DataTypes.DATE
+  }, {});
+  Loans.associate = function(models) {
+    // associations can be defined here
+  };
+  return Loans;
+};
