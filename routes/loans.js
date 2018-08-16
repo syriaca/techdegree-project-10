0c734817c19f2341650b9cdf78fc6749cde16d9d
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-16 10:58:06 +0200

    Added first main routes
    Added routes for main categories (patrons, loans and books)

--- routes/loans.js
+++ routes/loans.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Library Manager' });
+  res.render('Loans', { title: 'All Loans' });
 });
 
 module.exports = router;
--- routes/patrons.js
+++ routes/patrons.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('patrons', { title: 'All Patrons' });
 });
 
 module.exports = router;
