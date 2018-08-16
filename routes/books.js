6ef4914276eb5162fde4b8cedce274f74013faf8
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-08-16 15:23:07 +0200

    database stuck

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,17 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('books', { title: 'All Books', page: req.baseUrl });
+  models.Books.findAll().then(function(books){
+    res.render('books', { 
+      title: 'All Books', 
+      page: req.baseUrl,
+      books: books
+    });
+  }).catch(function(err){
+    res.send(500);
+  });
 });
 
 router.get('/new', function(req, res, next) {
--- routes/patrons.js
+++ routes/patrons.js
@@ -1,8 +1,15 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('patrons', { title: 'All Patrons' });
+  models.Patrons.findAll().then(function(patrons) {
+    res.render('patrons', {
+      title: 'All Patrons',
+      patron: patrons
+    });
+    console.log(patrons);
+  });
 });
 
 router.get('/new', function(req, res, next) {
